b5313370dc174d4b9147e02e28ce0deabf791bc1
Mukesh Selvaraj <abirami@ishamistygreen.com>
2022-12-22 13:02:31 +0530

    comp

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js', './modals/**/*.js'],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
